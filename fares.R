b398f0e03ff090c6aab27103a1b5561af94c3fa9
konanurk <konanurk@umich.edu>
2018-04-13 12:46:30 -0400

    fares

--- fares.R
+++ fares.R
@@ -1,1 +1,16 @@
 fares <- read.csv("fares.csv")
+fares$revenue_per_customer <- fares$revenue / fares$num_people
+summary(fares$revenue)
+
+library(dplyr)
+
+flights <- read.csv("PRIMARY_flight_traffic.csv")
+x <- flights %>% group_by(quarter, airline_id, origin_airport, destination_airport) %>% dplyr::count()
+
+fares <- left_join(fares, x, by = c("quarter", "airline_id", "origin_airport", "destination_airport")) %>% rename(num_flights = n)
+
+
+flights$X <- NULL
+flights$wheels_off <- NULL
+flights$wheels_on <- NULL
+
